31639eaf9cf4c8460c1c9f93f583cec04b2bd69f
Ryan Saladino <saladinoryan5@gmail.com>
2023-03-28 09:45:38 +0200

    Task manager done without css

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,7 +21,19 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.Find(params[:id])
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to task_path(task.id)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  resources :tasks, only: %i[index show new create edit update]
+  resources :tasks, only: %i[index show new create edit update destroy]
 end
